1f09771f97a4e7c5ea469535e421052d1138b35c
Miguel A. Contreras <myge.one@gmail.com>
2022-12-18 22:33:59 -0300

    update api 2 stage

--- API/app/main.py
+++ API/app/main.py
@@ -6,6 +6,8 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 import functools
 import sys
+import json
+from pydantic import BaseModel
 
 # import a function from another file 
 from ML_model.ds_ml import get_neighbors
@@ -55,3 +57,51 @@ def getSimilarSchools(id_school: int, id_level: int, comuna: int,response: Respo
         return res
     else:
         return res
+
+
+class Student(BaseModel):
+    id_school: int
+    id_level: int
+    siblingsInSchool: bool
+    isPriority: bool
+    isSonOfTeacher: bool
+    isExStudent: bool
+
+@app.post("/getProbabilitySecondStage",status_code=200)
+async def getSimilarSchools(student: Student,response: Response) -> Union[dict, int]:
+    # req to json
+    req = student.dict()
+    query = ""
+    id_school = req['id_school']
+    id_level = req['id_level']
+    siblingsInSchool = req['siblingsInSchool']
+    isPriority = req['isPriority']
+    isSonOfTeacher = req['isSonOfTeacher']
+    isExStudent = req['isExStudent']
+
+    try:
+        for year in (2021,2020,2019,2018):
+            query += "SELECT v.rbd,v.cod_nivel,v.vacantes_regular,'{year}' as ano,v.cod_espe, ( SELECT count (*) as postulaciones  FROM c1_{year} WHERE rbd = '{id_colegio}'AND cod_nivel = '{id_curso}' ) FROM A1_{year} v WHERE rbd = '{id_colegio}' AND cod_nivel = '{id_curso}' ".format(year=year,id_colegio = id_school,id_curso=id_level)
+            if year != 2018:
+                query += ' UNION '
+        cursor.execute(query)
+        res = pd.DataFrame(cursor.fetchall(),columns=['rbd','cod_nivel','vacantes_regular','ano','cod_espe','postulaciones'])
+        if(req['siblingsInSchool'] ):
+            res = (1 - ( int(res.iloc[0]['postulaciones']) / int(res.iloc[0]['vacantes_regular'])))
+        if( req['siblingsInSchool'] == 0):
+            if(req['isPriority'] ):
+                res = 0.75 - ( int(res.iloc[0]['postulaciones']) / int(res.iloc[0]['vacantes_regular']))
+        if( req['siblingsInSchool'] == 0 and req['isPriority'] == 0):
+            if(req['isSonOfTeacher'] ):
+                res = 0.5 - ( int(res.iloc[0]['postulaciones']) / int(res.iloc[0]['vacantes_regular']))
+        if( req['siblingsInSchool'] == 0 and req['isPriority'] == 0 and req['isSonOfTeacher'] == 0):
+            if(req['isExStudent'] ):
+                res = 0.25 - ( int(res.iloc[0]['postulaciones']) / int(res.iloc[0]['vacantes_regular']))
+        if( req['siblingsInSchool'] == 0 and req['isPriority'] == 0 and req['isSonOfTeacher'] == 0 and req['isExStudent'] == 0):
+            res = 0
+
+        return res
+
+    except:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"probability": -1}
